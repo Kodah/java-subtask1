a557542c0e60edc020725d6f929256e43ec72bd3
Kodah <tom_1440@hotmail.co.uk>
2015-10-08 20:48:59 +0100

    Add getters for book properties

--- src/library/Book.java
+++ src/library/Book.java
@@ -2,7 +2,6 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package library;
 
 /**
@@ -15,11 +14,20 @@ public class Book {
     private Member borrower = null;
     private static int bookCount = 0;
     private int accessionNumber;
+    private String iSBNNumber;
+    private String author;
 
-    public Book(String name){
+    public Book(String name) {
         title = name;
         accessionNumber = bookCount++;
 
+    }
+
+    public Book(String name, String author, String iSBNNumber) {
+        title = name;
+        this.author = author;
+        this.iSBNNumber = iSBNNumber;
+        accessionNumber = bookCount++;
     }
 
     void setBorrower(Member theBorrower) {
@@ -29,9 +37,25 @@ public class Book {
     Member getBorrower() {
         return borrower;
     }
-    
+
     String getTitle() {
         return title;
     }
+    
+    String getAuthor()
+    {
+        return author;
+    }
+    
+    String getISBNNumber()
+    {
+        return iSBNNumber;
+    }
+    
+    int getAccessionNumber()
+    {
+        return accessionNumber;
+    }
+    
 
 }
